eadd1b90ef4305898004462f938c953a4b3d7d40
Attila Szigeti <szigat@gmail.com>
2022-10-19 17:14:53 +0200

    Add rows and columns examples

--- QuestPdfOtherExamples/Examples/3_ALotofText.cs
+++ QuestPdfOtherExamples/Examples/3_ALotofText.cs
@@ -1,4 +1,6 @@
 ﻿using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +13,36 @@ namespace QuestPdfOtherExamples.Examples
     {
         public Document GetDocument()
         {
-            throw new NotImplementedException();
+            var document = Document.Create(container =>
+            {
+                container.Page(page =>
+                {
+                    page.Content()
+                        .Column(column =>
+                        {
+                            column.Spacing(10);
+                            for (int i = 0; i < 15; i++)
+                            {
+                                column.Item().Text(Placeholders.LoremIpsum());
+                            }
+
+                            column.Item().PageBreak();
+
+                            for (int i = 0; i < 15; i++)
+                            {
+                                column.Item().Row(row => {
+                                    row.RelativeItem().ShowEntire().Text(Placeholders.LoremIpsum()).FontColor(Colors.Blue.Medium).Strikethrough(false);
+                                });
+                                
+                            }
+                            
+                        });
+                });
+
+
+            });
+
+            return document;
         }
     }
 }
